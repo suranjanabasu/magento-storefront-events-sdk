da66e542cee4d5cd02558f2339bebce527bcefef
Ani Hammond <annhammo@adobe.com>
2022-02-15 09:56:43 -0600

    SEARCH-1838-2: add set and get context for new extension version contexts (#73)

--- src/ContextManager.ts
+++ src/ContextManager.ts
@@ -16,52 +16,34 @@ import {
     ReferrerUrl,
     SearchInput,
     SearchResults,
+    DataServicesExtension,
+    RecommendationsExtension,
+    SearchExtension,
 } from "./types/schemas";
 
 export default class ContextManager extends Base {
-    /**
-     * Get Adobe Experience Platform context
-     */
     getAEP(): AEP {
         return this.getContext<AEP>(contexts.AEP_CONTEXT);
     }
 
-    /**
-     * Set Adobe Experience Platform context
-     * @param context
-     */
     setAEP(context: AEP): void {
         this.setContext<AEP>(contexts.AEP_CONTEXT, context);
     }
 
-    /**
-     * Get category context
-     */
     getCategory(): Category {
         return this.getContext<Category>(contexts.CATEGORY_CONTEXT);
     }
 
-    /**
-     * Set category context
-     * @param context
-     */
     setCategory(context: Category): void {
         this.setContext<Category>(contexts.CATEGORY_CONTEXT, context);
     }
 
-    /**
-     * Get event forwarding context
-     */
     getEventForwarding(): EventForwarding {
         return this.getContext<EventForwarding>(
             contexts.EVENT_FORWARDING_CONTEXT,
         );
     }
 
-    /**
-     * Set Adobe Experience Platform context
-     * @param context
-     */
     setEventForwarding(context: EventForwarding): void {
         this.setContext<EventForwarding>(
             contexts.EVENT_FORWARDING_CONTEXT,
@@ -69,49 +51,33 @@ export default class ContextManager extends Base {
         );
     }
 
-    /**
-     * Get url context
-     */
     getCustomUrl(): CustomUrl {
         return this.getContext<CustomUrl>(contexts.CUSTOM_URL_CONTEXT);
     }
 
-    /**
-     * Set url context
-     * @param context
-     */
     setCustomUrl(context: CustomUrl): void {
         this.setContext<CustomUrl>(contexts.CUSTOM_URL_CONTEXT, context);
     }
 
-    /**
-     * Get shopper context
-     **/
-    getShopper(): Shopper {
-        return this.getContext<Shopper>(contexts.SHOPPER_CONTEXT);
+    getDataServicesExtension(): DataServicesExtension {
+        return this.getContext<DataServicesExtension>(
+            contexts.DATA_SERVICES_EXTENSION_CONTEXT,
+        );
     }
 
-    /**
-     * Set shopper context
-     * @param context
-     **/
-    setShopper(context: Shopper): void {
-        this.setContext<Shopper>(contexts.SHOPPER_CONTEXT, context);
+    setDataServicesExtension(context: DataServicesExtension): void {
+        this.setContext<DataServicesExtension>(
+            contexts.DATA_SERVICES_EXTENSION_CONTEXT,
+            context,
+        );
     }
 
-    /**
-     * Get Magento extension context
-     */
     getMagentoExtension(): MagentoExtension {
         return this.getContext<MagentoExtension>(
             contexts.MAGENTO_EXTENSION_CONTEXT,
         );
     }
 
-    /**
-     * Set Magento extension context
-     * @param context
-     */
     setMagentoExtension(context: MagentoExtension): void {
         this.setContext<MagentoExtension>(
             contexts.MAGENTO_EXTENSION_CONTEXT,
@@ -119,64 +85,36 @@ export default class ContextManager extends Base {
         );
     }
 
-    /**
-     * Get order context
-     */
     getOrder(): Order {
         return this.getContext<Order>(contexts.ORDER_CONTEXT);
     }
 
-    /**
-     * Set order context
-     * @param context
-     */
     setOrder(context: Order): void {
         this.setContext<Order>(contexts.ORDER_CONTEXT, context);
     }
 
-    /**
-     * Get page offset context
-     */
     getPage(): Page {
         return this.getContext<Page>(contexts.PAGE_CONTEXT);
     }
 
-    /**
-     * Set page offset context
-     * @param context
-     */
     setPage(context: Page): void {
         this.setContext<Page>(contexts.PAGE_CONTEXT, context);
     }
 
-    /**
-     * Get product context
-     */
     getProduct(): Product {
         return this.getContext<Product>(contexts.PRODUCT_CONTEXT);
     }
 
-    /**
-     * Set product context
-     * @param context
-     */
     setProduct(context: Product): void {
         this.setContext<Product>(contexts.PRODUCT_CONTEXT, context);
     }
 
-    /**
-     * Get recommendations context
-     */
     getRecommendations(): Recommendations {
         return this.getContext<Recommendations>(
             contexts.RECOMMENDATIONS_CONTEXT,
         );
     }
 
-    /**
-     * Set recommendations context
-     * @param context
-     */
     setRecommendations(context: Recommendations): void {
         this.setContext<Recommendations>(
             contexts.RECOMMENDATIONS_CONTEXT,
@@ -184,47 +122,52 @@ export default class ContextManager extends Base {
         );
     }
 
-    /**
-     * Get referrer url context
-     */
+    getRecommendationsExtension(): RecommendationsExtension {
+        return this.getContext<RecommendationsExtension>(
+            contexts.RECOMMENDATIONS_EXTENSION_CONTEXT,
+        );
+    }
+
+    setRecommendationsExtension(context: RecommendationsExtension): void {
+        this.setContext<RecommendationsExtension>(
+            contexts.RECOMMENDATIONS_EXTENSION_CONTEXT,
+            context,
+        );
+    }
+
     getReferrerUrl(): ReferrerUrl {
         return this.getContext<ReferrerUrl>(contexts.REFERRER_URL_CONTEXT);
     }
 
-    /**
-     * Set referrer url context
-     * @param context
-     */
     setReferrerUrl(context: ReferrerUrl): void {
         this.setContext<ReferrerUrl>(contexts.REFERRER_URL_CONTEXT, context);
     }
 
-    /**
-     * Get search input context
-     */
+    getSearchExtension(): SearchExtension {
+        return this.getContext<SearchExtension>(
+            contexts.SEARCH_EXTENSION_CONTEXT,
+        );
+    }
+
+    setSearchExtension(context: SearchExtension): void {
+        this.setContext<SearchExtension>(
+            contexts.SEARCH_EXTENSION_CONTEXT,
+            context,
+        );
+    }
+
     getSearchInput(): SearchInput {
         return this.getContext<SearchInput>(contexts.SEARCH_INPUT_CONTEXT);
     }
 
-    /**
-     * Set search input context
-     * @param context
-     */
     setSearchInput(context: SearchInput): void {
         this.setContext<SearchInput>(contexts.SEARCH_INPUT_CONTEXT, context);
     }
 
-    /**
-     * Get search results context
-     */
     getSearchResults(): SearchResults {
         return this.getContext<SearchResults>(contexts.SEARCH_RESULTS_CONTEXT);
     }
 
-    /**
-     * Set search results context
-     * @param context
-     */
     setSearchResults(context: SearchResults): void {
         this.setContext<SearchResults>(
             contexts.SEARCH_RESULTS_CONTEXT,
@@ -232,34 +175,28 @@ export default class ContextManager extends Base {
         );
     }
 
-    /**
-     * Get shopping cart context
-     */
+    getShopper(): Shopper {
+        return this.getContext<Shopper>(contexts.SHOPPER_CONTEXT);
+    }
+
+    setShopper(context: Shopper): void {
+        this.setContext<Shopper>(contexts.SHOPPER_CONTEXT, context);
+    }
+
     getShoppingCart(): ShoppingCart {
         return this.getContext<ShoppingCart>(contexts.SHOPPING_CART_CONTEXT);
     }
 
-    /**
-     * Set shopping cart context
-     * @param context
-     */
     setShoppingCart(context: ShoppingCart): void {
         this.setContext<ShoppingCart>(contexts.SHOPPING_CART_CONTEXT, context);
     }
 
-    /**
-     * Get storefront instance context
-     */
     getStorefrontInstance(): StorefrontInstance {
         return this.getContext<StorefrontInstance>(
             contexts.STOREFRONT_INSTANCE_CONTEXT,
         );
     }
 
-    /**
-     * Set storefront instance context
-     * @param context
-     */
     setStorefrontInstance(context: StorefrontInstance): void {
         this.setContext<StorefrontInstance>(
             contexts.STOREFRONT_INSTANCE_CONTEXT,
@@ -267,19 +204,10 @@ export default class ContextManager extends Base {
         );
     }
 
-    /**
-     * Get custom context
-     * @param name
-     */
     getContext<T>(name?: string): T {
         return super.getContext<T>(name);
     }
 
-    /**
-     * Set custom context
-     * @param name
-     * @param context
-     */
     setContext<T>(name: string, context: T): void {
         super.setContext<T>(name, context);
     }
--- src/types/contexts.ts
+++ src/types/contexts.ts
@@ -3,13 +3,16 @@ import {
     AEP,
     Category,
     CustomUrl,
+    DataServicesExtension,
     EventForwarding,
     MagentoExtension,
     Order,
     Page,
     Product,
     Recommendations,
+    RecommendationsExtension,
     ReferrerUrl,
+    SearchExtension,
     SearchInput,
     SearchResults,
     Shopper,
@@ -21,13 +24,16 @@ export type ContextName =
     | typeof contexts.AEP_CONTEXT
     | typeof contexts.CATEGORY_CONTEXT
     | typeof contexts.CUSTOM_URL_CONTEXT
+    | typeof contexts.DATA_SERVICES_EXTENSION_CONTEXT
     | typeof contexts.EVENT_FORWARDING_CONTEXT
     | typeof contexts.MAGENTO_EXTENSION_CONTEXT
     | typeof contexts.ORDER_CONTEXT
     | typeof contexts.PAGE_CONTEXT
     | typeof contexts.PRODUCT_CONTEXT
     | typeof contexts.RECOMMENDATIONS_CONTEXT
+    | typeof contexts.RECOMMENDATIONS_EXTENSION_CONTEXT
     | typeof contexts.REFERRER_URL_CONTEXT
+    | typeof contexts.SEARCH_EXTENSION_CONTEXT
     | typeof contexts.SEARCH_INPUT_CONTEXT
     | typeof contexts.SEARCH_RESULTS_CONTEXT
     | typeof contexts.SHOPPER_CONTEXT
@@ -38,13 +44,16 @@ export type Context = {
     [contexts.AEP_CONTEXT]?: AEP;
     [contexts.CATEGORY_CONTEXT]: Category;
     [contexts.CUSTOM_URL_CONTEXT]: CustomUrl;
+    [contexts.DATA_SERVICES_EXTENSION_CONTEXT]: DataServicesExtension;
     [contexts.EVENT_FORWARDING_CONTEXT]?: EventForwarding;
     [contexts.MAGENTO_EXTENSION_CONTEXT]: MagentoExtension;
     [contexts.ORDER_CONTEXT]: Order;
     [contexts.PAGE_CONTEXT]: Page;
     [contexts.PRODUCT_CONTEXT]: Product;
+    [contexts.RECOMMENDATIONS_EXTENSION_CONTEXT]: RecommendationsExtension;
     [contexts.RECOMMENDATIONS_CONTEXT]: Recommendations;
     [contexts.REFERRER_URL_CONTEXT]: ReferrerUrl;
+    [contexts.SEARCH_EXTENSION_CONTEXT]: SearchExtension;
     [contexts.SEARCH_INPUT_CONTEXT]: SearchInput;
     [contexts.SEARCH_RESULTS_CONTEXT]: SearchResults;
     [contexts.SHOPPER_CONTEXT]: Shopper;
